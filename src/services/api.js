8f11f796799542dfe61d16f114c57a7f151441e0
Tej7278-chippada <tejachippada45@gmail.com>
2025-11-14 22:30:55 +0530

    ip based local hosting implemented for ensuring server connection to mobile devices 👍

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000'; // Flask backend URL
+const API_BASE_URL = 'http://192.168.192.172:8000'; // Flask backend URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
